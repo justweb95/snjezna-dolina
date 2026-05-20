2b14c7b5b04ae601fce4164918f3469535e7a12d
Pavle Pesic <p.pavle16@gmail.com>
2026-05-21 00:22:34 +0200

    Started working on hero section

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,13 +1,47 @@
 <script setup>
-
+  import logo from '@/assets/images/snjezna-dolina-logo-text.svg'
+  import phoneIcon from '@/assets/images/home/phone-icon.svg'
+  import hamburgerMenu from '@/assets/images/home/hamburger-menu.svg'
+  
 </script>
 
 <template>
   <header class="header-main">
-    
+    <a href="#">
+      <img
+        :src="logo"
+        alt="Snjezna Dolina logo"
+        class="logo"/>
+    </a>
+    <div class="icon-holder">
+      <a href="tel:+1234567890">
+        <img 
+          :src="phoneIcon" 
+          alt="Phone icon" 
+          class="phone-icon"/>
+      </a>
+      <a href="#">
+        <img 
+          :src="hamburgerMenu" 
+          alt="Hamburger menu" 
+          class="hamburger-menu"/>
+      </a>
+    </div>
   </header>
 </template>
 
-<style lang="scss" scoped>
-
+<style scoped>
+  .header-main {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 16px;
+    .logo {
+      max-width: 100%;
+    }
+    .icon-holder {
+      display: flex;
+      gap: 8px;
+    }
+  }
 </style>
--- src/sections/HeroSection.vue
+++ src/sections/HeroSection.vue
@@ -1,13 +1,97 @@
 <script setup>
+  // Image Imports
+  import Header from '../layout/Header.vue';
+  import locationBadge from '../assets/images/home/location-badge.svg';
+  import mapIcon from '../assets/images/home/map-icon.svg';
+  import heroImage from '../assets/images/home/hero-image.webp';
+  
 
 </script>
 
 <template>
   <section class="hero-section">
-    <h1>Hero Section</h1>
+    <div class="doli-container">
+      <div class="hero-content">
+        <Header />
+        <span class="badge">
+          <img 
+            :src="locationBadge" 
+            alt="Location badge"
+            class="badge-icon"/>
+          Jahorina, BIH
+        </span>
+        <h1 class="hero-title">Apartmani na Jahorini <br> Uz samu ski stazu</h1>
+        <p class="sub-content">Boravak već od 60 € po noćenju</p>
+        
+      </div>
+      
+      <div class="hero-image-holder">
+        <img 
+          :src="heroImage" 
+          alt="Hero image"
+          class="hero-img"/>
+
+          <img 
+          :src="mapIcon" 
+          alt="Map icon"
+          class="map-icon"/>
+      </div>
+    </div>
   </section>
 </template>
 
 <style scoped>
+  .hero-section {
+    padding: 24px 0px;
+    .doli-container {
+      display: flex;
+      gap: 24px;
+      .hero-content {
+        flex: 1 0 49%;
+        max-width: 768px;
+        display: flex;
+        flex-direction: column;
+        .badge {
+          width: fit-content;
+          margin: 0px auto;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 8px 24px;
+          gap: 12px;
+          border: 1px solid #E4E9ED;
+          border-radius: 40px;
+        }
+        .hero-title {
+          margin: 24px 0 28px;
+          text-transform: uppercase;
+          text-align: center;
+        }
+        .sub-content {
+          font-weight: 700;
+          font-size: 18px;
+          line-height: 23px;
+          text-align: center;
+        }
+      }
+      .hero-image-holder {
+        position: relative;
+        flex: 1 0 49%;
+        max-width: 768px;
 
+        border-radius: 20px;
+        overflow: hidden;
+        .hero-img {
+          width: 100%;
+          height: auto;
+          display: block;
+        }
+        .map-icon {
+          position: absolute;
+          bottom: 32px;
+          right: 32px;
+        }
+      }
+    }
+  }
 </style>
