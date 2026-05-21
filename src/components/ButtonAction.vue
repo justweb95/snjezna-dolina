a40c9578da594dac057e135120ccab6ffdf98fb9
Pavle Pesic <p.pavle16@gmail.com>
2026-05-21 23:46:06 +0200

    Hero on 70% done

--- src/components/ButtonAction.vue
+++ src/components/ButtonAction.vue
@@ -5,17 +5,73 @@
     type: {
       type: String,
       default: 'primary',
-      validator: (value: string) => ['primary', 'secondary'].includes(value),
+      validator: (value) => ['primary', 'secondary'].includes(value),
     },
   });
+
+  import buttonIcon from '../assets/images/home/button-icon.svg';
+  import buttonIconWhite from '../assets/images/home/button-icon-white.svg';
 
 
 </script>
 
 <template>
-  
+  <a :href="btnProps.action" :class="['btn', btnProps.type]">
+    <img
+     :src="btnProps.type === 'primary' ? buttonIcon : buttonIconWhite" 
+     class="button-icon"
+     alt="Button icon" />
+    <span class="btn-text">{{ btnProps.text }}</span>
+  </a>
 </template>
 
-<style lang="scss" scoped>
+<style scoped>  
 
+.btn {
+  display: flex;
+  width: fit-content;
+  align-items: center;
+  gap: 24px;
+
+  padding: 4px 36px 4px 4px;
+  border-radius: 999px;
+
+  font-weight: 600;
+  text-decoration: none;
+
+  transition: background-color .7s ease, color 0.3s ease;
+
+  .btn-text {
+    transition: transform .7s cubic-bezier(0.22, 1, 0.36, 1);
+  }
+
+  .button-icon {
+    border-radius: 50%;
+    background-color: #fff;
+    padding: 14px 18px;
+
+    transition: transform .7s cubic-bezier(0.22, 1, 0.36, 1);
+  }
+
+  &.primary {
+    background-color: #13202E;
+    color: #fff;
+
+    &:hover {
+      background-color: #2FB6EF;
+      color: #fff;
+    }
+
+    /* ICON goes right */
+    &:hover .button-icon {
+      transform: translateX(222px) rotate(360deg);
+    }
+
+    /* TEXT moves left */
+    &:hover .btn-text {
+      transform: translateX(-50px);
+    }
+  }
+}
+  
 </style>
--- src/sections/HeroSection.vue
+++ src/sections/HeroSection.vue
@@ -4,6 +4,10 @@
   import locationBadge from '../assets/images/home/location-badge.svg';
   import mapIcon from '../assets/images/home/map-icon.svg';
   import heroImage from '../assets/images/home/hero-image.webp';
+  import guestFavorite from '../assets/images/home/guest-favorite.svg';
+
+  // Button imports
+  import ButtonAction from '../components/ButtonAction.vue';
   
 
 </script>
@@ -13,6 +17,7 @@
     <div class="doli-container">
       <div class="hero-content">
         <Header />
+        
         <span class="badge">
           <img 
             :src="locationBadge" 
@@ -22,7 +27,33 @@
         </span>
         <h1 class="hero-title">Apartmani na Jahorini <br> Uz samu ski stazu</h1>
         <p class="sub-content">Boravak već od 60 € po noćenju</p>
-        
+        <ButtonAction 
+          type="primary"
+          text="Pogledajte apatmane"
+          action="#apartmans"
+        />
+
+        <p class="hero-text">Doživite pravu zimsku čaroliju! Naši moderni apartmani nalaze se tik uz ski lift – izađite iz smeštaja i odmah ste na stazi.</p>
+        <div class="hero-score">
+          <img class="guest-favorite"
+            :src="guestFavorite" 
+            alt="Guest favorite icon"
+          />
+          <span class="guest-return">
+            <h3>80%</h3>
+            <p>Povratak</p>
+          </span>
+          <hr>
+          <span class="guest-return">
+            <h3>4.9</h3>
+            <p>Ocena</p>
+          </span>
+          <hr>
+          <span class="guest-return">
+            <h3>24/7</h3>
+            <p>Podrška</p>
+          </span>
+        </div>
       </div>
       
       <div class="hero-image-holder">
@@ -53,7 +84,7 @@
         flex-direction: column;
         .badge {
           width: fit-content;
-          margin: 0px auto;
+          margin: auto auto 0;
           display: flex;
           justify-content: center;
           align-items: center;
@@ -72,6 +103,34 @@
           font-size: 18px;
           line-height: 23px;
           text-align: center;
+        }
+        .hero-text {
+          text-align: center;
+          margin-top: auto;
+        }
+        .hero-score {
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          gap: 32px;
+          padding: 24px 32px;
+          border: 1px solid #E4E9ED;
+          border-radius: 20px;
+          .guest-favorite {
+            margin-right: auto;
+          }
+          .guest-return {
+            h3 {
+              font-size: 18px;
+              line-height: 32px;
+              font-weight: 700;
+            }
+            p {
+              font-size: 12px;
+              line-height: auto;
+              font-weight: 400;
+            }
+          }
         }
       }
       .hero-image-holder {
